eee4dc582449652d7326438b9f0a431e78337a26
Leon Heuer <leon.heuer@outlook.de>
2022-02-03 17:57:57 +0100

    Add particles

--- src/main/java/de/nyc/valorant/listeners/PlayerInteract.java
+++ src/main/java/de/nyc/valorant/listeners/PlayerInteract.java
@@ -3,13 +3,12 @@ package de.nyc.valorant.listeners;
 import de.nyc.valorant.MCValorant;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.scheduler.BukkitScheduler;
-import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.util.Vector;
 
 public class PlayerInteract implements Listener {
@@ -23,20 +22,19 @@ public class PlayerInteract implements Listener {
     @EventHandler
     public void onInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
-        if(event.getMaterial().equals(Material.WOODEN_AXE)) {
-            if (event.getAction().equals(Action.RIGHT_CLICK_AIR) || event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
-                Location location_spell = player.getLocation();
-                Vector vector = location_spell.toVector();
-                BukkitScheduler scheduler = main.getServer().getScheduler();
+        if (player.getItemOnCursor().getType() != Material.WOODEN_AXE) {
+            return;
+        }
 
-                BukkitTask task = scheduler.runTaskTimer(main, () -> {
-                    vector.add(vector);
+        if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            Location loc = player.getLocation();
+            if (loc.getWorld() == null) {
+                return;
+            }
 
-                    //Objects.requireNonNull(player.getLocation().getWorld()).spawnParticle(Particle.COMPOSTER, location_spell, 1);
-                    //location_spell = vector.toLocation(player.getWorld());
-                }, 2L, 2L);
-
-                scheduler.runTaskLater(main, task::cancel, 20 * 5L);
+            Vector direction = loc.getDirection().normalize();
+            for (int i = 0; i < 50; i++) {
+                loc.getWorld().spawnParticle(Particle.COMPOSTER, loc.add(direction), 1);
             }
         }
 
